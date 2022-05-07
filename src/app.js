de3faa887302c665a27da29f0d7fd45735fab859
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-05-07 06:56:51 +0530

    add custom cors handler

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express'
-import cors from 'cors'
 import router from './router/index.js'
 import TokenService from './service/TokenService.js'
 import {logger} from './logger/logger.js'
 import Games from './domain/Games.js'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json({limit: '1mb'}))
-app.use(cors())
+
+app.use(cors({
+  optionsSuccessStatus: 200,
+  methods: "GET, POST"
+}))
 
 app.use((req, res, next) => {
   const {send} = res
